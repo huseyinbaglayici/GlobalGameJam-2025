32038d89f9110ad091e63db2674b608a8e91e16d
huseyinbaglayici <huseyinow35@hotmail.com>
2025-01-25 17:12:43 +0300

    dash system 02

--- Assets/Scripts/Bubble.cs
+++ Assets/Scripts/Bubble.cs
@@ -4,43 +4,21 @@ using System.Collections.Generic;
 using Cysharp.Threading.Tasks;
 using DG.Tweening;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 public class Bubble : MonoBehaviour
 {
     private const string EnemyTag = "Enemy";
     private bool isEnemyCatched;
-    [SerializeField] private float bubbleDurationTime = 2.5f;
 
     public async void InitializeBubble()
     {
         CorrectedPosition();
 
-<<<<<<< Updated upstream
         transform.DOLocalMoveY(transform.position.y + 2.5f, 1f).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.Linear);
         //lifetime
-        await UniTask.WaitForSeconds(4f);
+        awa it UniTask.WaitForSeconds(4f);
         DOTween.Kill(transform);
         if (isEnemyCatched) return;
-=======
-        if (transform != null)
-        {
-            transform.DOLocalMoveY(transform.position.y + 2.5f, 1f).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.Linear);
-        }
-
-        // Lifetime (4 saniye bekleme)
-        await UniTask.WaitForSeconds(bubbleDurationTime);
-
-        if (this == null || gameObject == null)
-        {
-            return;
-        }
-
-
-        if (isEnemyCatched) return;
-        DOTween.Kill(transform);
-
->>>>>>> Stashed changes
         Destroy(gameObject);
     }
 
@@ -58,20 +36,12 @@ public class Bubble : MonoBehaviour
             isEnemyCatched = true;
             other.transform.SetParent(transform);
             other.transform.GetComponent<BoxCollider2D>().enabled = false;
-
-            if (other.transform != null)
+            other.transform.DOLocalMove(Vector3.zero, 1f);
+            transform.DOScale(0, 1f).SetEase(Ease.InBounce).OnComplete(() =>
             {
-                other.transform.DOLocalMove(Vector3.zero, 1f);
-            }
-
-            if (transform != null)
-            {
-                transform.DOScale(0, 1f).SetEase(Ease.InBounce).OnComplete(() =>
-                {
-                    isEnemyCatched = false;
-                    Destroy(gameObject);
-                });
-            }
+                Destroy(gameObject);
+                isEnemyCatched = false;
+            });
         }
     }
 }
